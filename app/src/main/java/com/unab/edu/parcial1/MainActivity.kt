5913af3d6a41cd9391813fee47e5af29143961f4
BetoCalderon <cp1307032018@unab.edu.sv>
2021-09-10 11:36:53 -0600

    actulizado

--- app/src/main/java/com/unab/edu/parcial1/MainActivity.kt
+++ app/src/main/java/com/unab/edu/parcial1/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Ejercicio 1 Realizar una función que tenga una sentencia ifelse-elseif en donde me compare el
+    // valor de la variable (Color) y de encontrarlo me muestre un mensaje en consola que diga el color de la
+    // variable y el color no existiera que me muestre un mensaje no se identifico el color. colocar el enlace
+    // al repositorio de GitHub
+
+        var color1 = rojo
+        var color2 = verde
+        var color3 = negro
+        
     }
 }
